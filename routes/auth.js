55558b240c443b761ac3315e11dae431cc396f98
code_with_nobi <7208851286sv@gmail.com>
2021-03-02 05:54:53 +0530

    Token update

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,10 @@ router.post('/login', (req, res, next) => {
     console.log(req.body,email, password);
     // Filter user from the users array by username and password
     // const user = users.find(u => { return u.username === username && u.password === password });
-    const user = pool.query("Select * from public.users WHERE email= $1 AND password = $2", [req.body.email,req.body.password]
-    // , (err, res) =>{
-    //     if(err) return next(err);
+    user = pool.query("Select * from public.users WHERE email= $1 AND password = $2", 
+    [email,password])
+    .then((rows,err) =>{
+        if(err) return next(err);
 
     //     if(res.rowCount === 0){
     //         response.json({"message":"No User Found"});
@@ -30,25 +31,42 @@ router.post('/login', (req, res, next) => {
     //             // role: user.role 
     //         }, secret);
     //         console.log(accessToken);
-
-            
     //     //    return  response.json({
     //     //         "token":accessToken}
     //     //     );
     //     // response.json(res.rows);
     // }
-    //   }
+            
+           
+        // Generate an access token
+        const accessToken = jwt.sign({ username: rows.username,}, secret);
+        console.log(rows.rows[0].id, accessToken);
+            pool.query(`UPDATE public.users SET auth_token = ($1) WHERE id =($2)`,
+        
+            [accessToken, rows.rows[0].id]).then(data =>{
+                console.log(data.rows);
+                pool.query("Select * from public.users WHERE id = $1", [rows.rows[0].id], (err, res) =>{
+                    if(err) return next(err);
+                    if(res.rowCount === 0)
+                    {
+                        res.json({"message":"No User Found"});
+                    }
+                    else
+                    {
+                        response.redirect(User.);
+                }
+              });
+            });
+        // res.json({
+        //     accessToken
+        // });
+
+      }
       );
 
     if (user) {
-        // Generate an access token
-        const accessToken = jwt.sign({ username: user.username,
-            role: user.role }, secret);
-
-        res.json({
-            accessToken
-        });
-    } else {
+    }
+     else {
         res.send('Username or password incorrect');
     }
 });
