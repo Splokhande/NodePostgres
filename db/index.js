abcb21754342e3681a2538d38fb6f596959075b3
code_with_nobi <39794708+Splokhande@users.noreply.github.com>
2021-04-04 02:32:49 +0530

    test

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const {Pool} = require('pg');
 const {user, host, database, password, port}= require('../db_config/config.js');
 
-const pool = new Pool({user, host, database, password, port,ssl:true});
+const pool = new Pool({user, host, database, password, port,ssl:{ rejectUnauthorized: false }});
 
 module.exports = pool;
