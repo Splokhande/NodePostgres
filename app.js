660f312f2c70cb4e342f6c5daa3047cb473ab932
code_with_nobi <39794708+Splokhande@users.noreply.github.com>
2021-04-04 02:06:53 +0530

    test

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const app = express();
 app.use((err,req,res,next) =>{
         res.json(err);
     });
-    
+app.get("/",(req,res)=>{
+    res.send("Hello World");
+});
 app.use(bodyParser.json());
 app.use("/user", userDetail);
 app.use("/auth", auth);
