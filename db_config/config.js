d9736a7c32097a5abf63b5c99a716db63bd21080
code_with_nobi <39794708+Splokhande@users.noreply.github.com>
2021-04-04 01:21:38 +0530

    heroku config

--- db_config/config.js
+++ db_config/config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  // host: "ec2-3-222-11-129.compute-1.amazonaws.com",
-  // user: "wspkmzuetvqdgq",
-  // password: "3daa69013cef2085e882adae68af02ec20e5e4b2ab1b268a3339fd449cf24029",
-  // database: "d8atboqf58jmn5",
-  host: "localhost",
-  user: "postgres",
-  password: "Sail8474",
-  database: "thehj",
-  port: 5432,
+  host: "ec2-54-235-108-217.compute-1.amazonaws.com",
+  user: "ownumxfkwgkmyt",
+  password: "255a5621b9e45b37bb4f1b1ea0fb9ed163e3714a8147fa26c4dd4b30b1ea732f",
+  database: "ddgphpmj7ue7b4",
+  // host: "localhost",
+  // user: "postgres",
+  // password: "Sail8474",
+  // database: "thehj",
+  // port: 5432,
   secret: 'naksld1j5ais2hia8sdm5nakldn56aitiwgtiw7jjgjndsghaahbfjkabfafnfajfjjaa84siufd549dlk',
   salt:3  
 };
