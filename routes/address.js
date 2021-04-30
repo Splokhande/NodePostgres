73352a1be232908b06efd2c8cfc5fc831897001a
code_with_nobi <39794708+Splokhande@users.noreply.github.com>
2021-05-01 01:13:02 +0530

    update address

--- routes/address.js
+++ routes/address.js
@@ -5,7 +5,7 @@ const pool = require("../db");
 ///country  
   router.post('/add/country', (request,response, next) => {
     const {country} = request.body;
-    pool.query('INSERT INTO country (country) VALUES ($1)' ,
+    pool.query('INSERT INTO country (country) VALUES ($1) Returning *' ,
     [country], (err, res) =>{
         if(err) return next(err);
         console.log("New Country Added: ",res.rowCount);
@@ -56,7 +56,7 @@ router.post('/add/state', (request,response, next) =>{
 router.post('/add/district', (request,response, next) =>{
  
   const {state_id,district} = request.body;
-  pool.query('INSERT INTO district (state_id, district) VALUES ($1, $2,) where id = ' ,
+  pool.query('INSERT INTO district (state_id, district) VALUES ($1, $2,) Returning * ' ,
   [state_id,district], (err, res) =>{
       if(err) return next(err);
       console.log("New District Added: ",res.rowCount);
@@ -83,7 +83,7 @@ router.post('/add/district', (request,response, next) =>{
 ///city
 router.post('/add/city', (request,response, next) =>{
   const {district_id,city} = request.body;
-  pool.query('INSERT INTO city (city, district_id) VALUES ($1, $2)' ,
+  pool.query('INSERT INTO city (city, district_id) VALUES ($1, $2) Returning *' ,
   [city, district_id], (err, res) =>{
       if(err) return next(err);
       console.log("New City Added: ",res.rowCount);
@@ -96,7 +96,7 @@ router.post('/add/city', (request,response, next) =>{
   router.get('/get/city/:id', (request,response, next) =>{
     const {id} = request.params;
     // pool.query("Select city.city_id, city.city, mc_list.mc_id, mc_list.mc ,district.district_id,district.district, state.state_id, state.state, state.country_id, country.country from state INNER JOIN country on state.country_id = country.country_id inner join district on district.state_id = state.state_id INNER JOIN mc_list on mc_list.district_id = district.district_id inner join city on mc_list.mc_id = city.city_id", (err, res) =>{
-      pool.query("Select * from city where district_id = $1",[id], (err, res) =>{
+      pool.query("Select * from city where district_id = $1 ",[id], (err, res) =>{
         if(err) return next(err);
         response.json({
            "message":"success",
@@ -127,7 +127,7 @@ router.put('/update/city/:id',(request, response, next) =>{
 ///area
 router.post('/add/area', (request,response, next) => {
   const {area , city_id} = request.body;
-  pool.query('INSERT INTO area (area, city_id) VALUES ($1, $2)' ,
+  pool.query('INSERT INTO area (area, city_id) VALUES ($1, $2) Returning *' ,
   [area,city_id], (err, res) =>{
       if(err) return next(err);
       console.log("New Area Added: ",res.rowCount);
@@ -173,7 +173,7 @@ router.put('/update/area/:id',(request, response, next) =>{
 ///municipal_corporation
 router.post('/add/mc', (request,response, next) =>{
   const {mc, country_id} = request.body;
-  pool.query('INSERT INTO mc_list (mc,country_id) VALUES ($1, $2)' ,
+  pool.query('INSERT INTO mc_list (mc,country_id) VALUES ($1, $2) Returning *' ,
   [mc, country_id], (err, res) =>{
       if(err) return next(err);
       console.log("New Municipal Corporation Added: ",res.rowCount);
@@ -217,7 +217,7 @@ router.put('/update/mc/:id',(request, response, next) =>{
 ///ward
 router.post('/add/ward', (request,response, next) =>{
 const {ward_no, mc_id ,country_id, nagarsevak} = request.body;
-pool.query('INSERT INTO wards (ward_no, mc_id , nagarsevak, country_id) VALUES ($1, $2, $3, $4)' ,
+pool.query('INSERT INTO wards (ward_no, mc_id , nagarsevak, country_id) VALUES ($1, $2, $3, $4) Returning *' ,
 [ward_no, mc_id , nagarsevak, country_id], (err, res) =>{
     if(err) return next(err);
     console.log("New Ward Added: ",res.rowCount);
@@ -261,11 +261,11 @@ router.put('/update/ward/:id',(request, response, next) =>{
 
 ///Address
 router.post('/add/address', (request,response, next) =>{
-  const {ward_no, mc_id ,country_id, nagarsevak} = request.body;
-  pool.query('INSERT INTO address (ward_no, mc_id , nagarsevak, country_id) VALUES ($1, $2, $3, $4)' ,
-  [ward_no, mc_id , nagarsevak, country_id], (err, res) =>{
+  const {ward,mc,area,city,district,state,country} = request.body;
+  pool.query('INSERT INTO address (ward,mc,area,city,district,state,country) VALUES ($1, $2, $3, $4, $5, $6, $7) Returning *' ,
+  [ward,mc,area,city,district,state,country], (err, res) =>{
       if(err) return next(err);
-      console.log("New Ward Added: ",res.rowCount);
+      console.log("New Address Added: ",res.rowCount);
       response.json({
            "message":"success",
             "data":res.rows
@@ -273,12 +273,25 @@ router.post('/add/address', (request,response, next) =>{
     });
   });
   
-  router.put('/update/ward',(request, response, next) =>{
+  router.put('/update/address/:id',(request, response, next) =>{
     const {id} = request.params;
   
-    const keys = ['ward','mc_id','country_id', 'nagarsevak'];
+    const keys = ['ward','mc','area','city','district','state','country'];
     const fields = [];
-   
+
+    keys.forEach(key =>{
+      if(request.body[key]) fields.push(key);
+  });
+
+  fields.forEach((field, index) =>{
+      pool.query(`UPDATE address SET ${field} = ($1) WHERE id =($2) Returning *`,
+      [request.body[field], id], (err, res) =>{
+          if(err) return next(err);
+            if(index === fields.length - 1)
+            response.status(200).json({"data":res.rows[0]});
+        });
+      });
+
   });
 
   router.get('/get/address/:district', (request,response, next) =>{
