c4f7e353e5b95c0d2200bb1350dee87b0ea13398
saiprasad.l <39794708+Splokhande@users.noreply.github.com>
2021-06-29 02:11:33 +0530

    update socBody

---
+++ routes/societyBody.js
@@ -0,0 +1,27 @@
+
+
+const {Router, request, response} = require("express");
+const router = Router();
+const pool = require("../db");
+const checkJWT = require('./authenticateAdmin');
+
+router.put("update/socBody",checkJWT, async(request,response,next)=>{
+    const {id} = request.params;
+    const keys = ['chairman', 'secretary', 'treasurer', 'vice_chairman','vice_secretary', 'vice_treasurer','members'];
+    const fields = [];
+    keys.forEach(key =>{
+        if(request.body[key]) fields.push(key);
+    });
+    fields.forEach((field, index) =>{
+        pool.query(`UPDATE public.soc_body SET ${field} = ($1) WHERE soc_body_id =($2) Returning *`,
+        [request.body[field], id], (err, res) =>{
+            if(err){
+              res.status(500);
+              return next(err);}
+              response.status(200).json({
+                "message":"success",
+                 "data":res.rows
+               });
+          });
+        });
+});
