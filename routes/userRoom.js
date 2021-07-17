949c2120a801fc7c23346c5519f0d6f86e236dd3
saiprasad.l <39794708+Splokhande@users.noreply.github.com>
2021-07-17 16:35:39 +0530

    useroom sell

--- routes/userRoom.js
+++ routes/userRoom.js
@@ -28,3 +28,41 @@ router.post('create/userRoom',(request,response,next)=>{
         });
     });
 });
+
+router.post('room/add/rental',(request,response,next)=>{
+    const {userId, roomId, socId,isResident,isRental} =request.body;
+
+    pool.query('update rooms set rental_id = array_append(rental_id,$1) where room_id =1 AND soc_id =1',[userId],(err,room)=>{
+        if(err) return next(err);
+        pool.query('Insert into user_room (soc_id,room_id,user_id,from_date,isResident,isRental) values ($1,$2,$3,$4,$5,$6,) Returning userroom_id',[socId,roomId,userId,day,isResident,isRental],(err,userRoom)=>{
+            if(err) return next(err);
+            var userRoomId = userRoom.rows[0].id;
+        pool.query('update users set user_room_id = array_append(user_room_id,$1) where id = $2',[userRoomId,userId],(err,users)=>{
+            if(err) return next(err);
+            response.sendStatus(200).json({
+                "message":success,
+                });
+            });
+        });
+    });
+});
+
+router.post('room/sell',(request,response,next)=>{
+    const {userRoomId,socId,roomId}=request.body;
+pool.query('update user_room set to_date = CURRENT_DATE where to_date is null AND soc_id = $1 AND room_id = $2',[socId, roomId],(err,userRoom)=>{
+    if(err) return next(err);
+    pool.query('update rooms set members_id = {} where soc_id = $1 AND room_id = $2',[socId,roomId],(err,rooms)=>{
+        if(err) return next(err);
+        pool.query('update users set user_room_id = array_remove(user_room_id,$1) where id = $2',[userRoomId,userId],(err,userR)=>{
+            if(err)return next(err);
+            response.sendStatus(200).json({
+                'message' : 'success',
+            });
+        });
+    });
+});
+
+
+});
+
+
