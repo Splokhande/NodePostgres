f5468c1c79d01f316453ae3947d400712b0fbb5d
saiprasad.l <39794708+Splokhande@users.noreply.github.com>
2021-09-12 13:23:41 +0530

    jwt

--- functions/errorHandling.js
+++ functions/errorHandling.js
@@ -19,7 +19,34 @@ class ErrorHandler extends Error {
       message
     });
   };
+  class ResponseHandler {
+      constructor(
+        statusCode,
+        //  message, 
+         result) {
+        // let msg = message;
+        // if(str.includes('duplicate key value violates unique constraint \"unique_mobile_no\"')){
+        //   msg = "This phone number is already registered.";
+        // }
+        super();
+        this.statusCode = statusCode;
+        // this.message = msg;
+        this.result = result;
+      }
+    }
+    const handleResponse = (data, res) => {
+      const {statusCode,
+        //  message,
+         result } = data;
+      res.status(statusCode).json({
+        status: "error",
+        statusCode,
+        result
+      });
+    };
   module.exports = {
     ErrorHandler,
-    handleError
+    handleError,
+    ResponseHandler,
+    handleResponse
   }
