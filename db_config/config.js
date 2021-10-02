962f982c479691f542a6b0d4db56ec552a263787
saiprasad.l <39794708+Splokhande@users.noreply.github.com>
2021-10-03 02:41:51 +0530

    admin authorisation

--- db_config/config.js
+++ db_config/config.js
@@ -1,22 +1,22 @@
 
 ///Local config
-module.exports = {
-  host: "localhost",
-  user: "postgres",
-  password: "Sail8474",
-  database: "thj_local",
-  port: 5432,
-  secret: 'naksld1j5ais2hia8sdm5nakldn56aitiwgtiw7jjgjndsghaahbfjkabfafnfajfjjaa84siufd549dlk',
-  salt:3,
-};
-// /// Server Config
 // module.exports = {
-//   host: "ec2-54-235-108-217.compute-1.amazonaws.com",
-//   user: "ownumxfkwgkmyt",
-//   password: "255a5621b9e45b37bb4f1b1ea0fb9ed163e3714a8147fa26c4dd4b30b1ea732f",
-//   database: "ddgphpmj7ue7b4",
+//   host: "localhost",
+//   user: "postgres",
+//   password: "Sail8474",
+//   database: "thj_local",
 //   port: 5432,
 //   secret: 'naksld1j5ais2hia8sdm5nakldn56aitiwgtiw7jjgjndsghaahbfjkabfafnfajfjjaa84siufd549dlk',
 //   salt:3,
-//  ssl:{ rejectUnauthorized: false }
 // };
+/// Server Config
+module.exports = {
+  host: "ec2-54-235-108-217.compute-1.amazonaws.com",
+  user: "ownumxfkwgkmyt",
+  password: "255a5621b9e45b37bb4f1b1ea0fb9ed163e3714a8147fa26c4dd4b30b1ea732f",
+  database: "ddgphpmj7ue7b4",
+  port: 5432,
+  secret: 'naksld1j5ais2hia8sdm5nakldn56aitiwgtiw7jjgjndsghaahbfjkabfafnfajfjjaa84siufd549dlk',
+  salt:3,
+ ssl:{ rejectUnauthorized: false }
+};
