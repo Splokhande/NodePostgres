391944513b499d9d34575b35a40c1596f51c5940
saiprasad.l <39794708+Splokhande@users.noreply.github.com>
2021-11-21 02:09:10 +0530

    image upload

--- functions/uploadPhoto.js
+++ functions/uploadPhoto.js
@@ -39,7 +39,7 @@ const dStorage = multer.diskStorage({
   },
 });
 
-const firebase = require('../db/firebasedb');
+// const firebase = require('../db/firebasedb');
 require("firebase/storage");
 const firebaseStore = admin.storage().bucket();
 // const upload = multer({ storage: dStorage }).single("file");
