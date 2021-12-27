035c00d0fa5c439111ac5cfbbf8b8f961066c1fe
saiprasad.l <39794708+Splokhande@users.noreply.github.com>
2021-12-27 15:13:11 +0530

    prod config

--- routes/auth.js
+++ routes/auth.js
@@ -14,69 +14,7 @@ const {secret} =  require('../db_config/config');
 const { success, error, validation } = require("../functions/response");
 var currentTimeInMilliseconds=new Date().toLocaleString(undefined, {timeZone: 'Asia/Kolkata'});
 
-router.post('/admin/login', async(request, response, next) => {
 
-    // Read username and password from request body
-    const { mobile_no, password,
-        device_id, mobile_model 
-    } = request.body;
-    console.log(request.body,mobile_no, password);
-    // Filter user from the users array by username and password
-    // const user = users.find(u => { return u.username === username && u.password === password });
-    await pool.query(`Select * from public.users WHERE mobile_no=$1`,[mobile_no])
-    .then(async(rows,err) =>{
-        if(err) return  next(new ErrorHandler(400, err.message));
-        // Generate an access token
-        let user = rows.rows[0];
-        console.log(rows.rows.length);
-        console.log(typeof rows.rows.length );
-        console.log(rows.rowCount > 0);
-        // const validPassword = await bcrypt.compare(password, rows.rows[0].password);
-        // console.log(validPassword);
-        console.log(rows.rows[0]);
-        if(Array.isArray(rows.rows) && rows.rows.length)
-      {
-        // if(rows.rows[0].password === validPassword){
-        if(rows.rows[0].password === password){
-          const accessToken = jwt.sign({ username: rows.rows[0].username,post: rows.rows[0].post}, secret);
-           
-          if( rows.rows[0].post == 'superadmin'){
-          // console.log(rows.rows[0].id, accessToken);
-                pool.query(`UPDATE public.users SET auth_token = ($1), device_id = ($2), mobile_model = ($3) WHERE id =($4)`,
-                [accessToken, device_id, mobile_model, rows.rows[0].id]).then((data, err) =>{
-                    // console.log(data.rows);
-                    response.json(
-                      success(
-                      "OK",
-                      {
-                          "user":rows.rows[0]},
-                      data.statusCode
-                  ));
-                });
-              }
-              else{
-                response.status(400).json(  success(
-                  "Unauthorised user",
-                  {},
-                      response.statusCode
-              ));
-              }
-        }else{
-            response.status(400).json({ error: "Invalid Password" });
-        }
-    }
-        else{
-            response.status(400).json({ error: "No User found"});
-        }
-      }
-      );
-  
-    // if (user) {
-    // }
-    //  else {
-    //     res.send('Username or password incorrect');
-    // }
-  });
   
 
 router.post('/login', async(request, response, next) => {
